46b942b070a86a7ca98e145664d76ac4a1b829ce
Julian Rosse <julian@helixbass.net>
2025-12-16 10:57:55 -0500

    working on seed

--- broken-antler/src/lib.rs
+++ broken-antler/src/lib.rs
@@ -1,5 +1,27 @@
+use sauvignon::{schema, Schema};
+
 mod database;
 mod types;
 
-pub use database::{Database, DatabaseCobbler, Event, VenuesCobbler};
+pub use database::{get_database, Database, DatabaseCobbler, Event, VenuesCobbler};
 pub use types::Venue;
+
+pub fn get_schema() -> Schema {
+    schema! {
+        types => [
+            Venue => {
+                fields => [
+                    name => string_column()
+                ]
+            }
+        ]
+        query => [
+            venues => {
+                type => [Venue!]!
+                internal_dependencies => [
+                    ids => id_column_list()
+                ]
+            }
+        ]
+    }
+}
