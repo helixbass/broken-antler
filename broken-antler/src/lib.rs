4beee199dd9b57840dab4b78f1fcf89f48b48503
Julian Rosse <julian@helixbass.net>
2025-12-15 19:54:01 -0500

    working on seed

---
+++ broken-antler/src/lib.rs
@@ -0,0 +1,72 @@
+use std::collections::HashSet;
+
+use geoutils::Location;
+use indexmap::IndexMap;
+use juriji::{CobbleAll, CreateEvent, EventForInsertion, ReadEvent};
+use serde::{Deserialize, Serialize};
+use squalid::_d;
+use uuid::Uuid;
+
+pub enum Event {
+    InsertVenue(Venue),
+}
+
+impl CreateEvent for Event {
+    fn create(&self) -> EventForInsertion {
+        match self {
+            Self::InsertVenue(venue) => EventForInsertion::new(
+                Some(venue.id),
+                "INSERT_VENUE".to_owned(),
+                to_serde_json_value_without_id(venue),
+            ),
+        }
+    }
+}
+
+fn to_serde_json_value_without_id<TSerializable: Serialize>(
+    value: &TSerializable,
+) -> serde_json::Value {
+    let mut value = serde_json::to_value(value).unwrap();
+    let _ = value.as_object_mut().unwrap().remove("id").unwrap();
+    value
+}
+
+#[derive(Deserialize)]
+pub struct Venue {
+    pub id: Uuid,
+    pub name: String,
+    pub location: Location,
+}
+
+impl CobbleAll for Venue {
+    fn relevant_event_types(&self) -> HashSet<String> {
+        ["INSERT_VENUE".to_owned()].into_iter().collect()
+    }
+
+    fn cobble(events: &[ReadEvent]) -> Vec<Self> {
+        let mut ret: IndexMap<Uuid, Self> = _d();
+        for event in events {
+            match &*event.type_ {
+                "INSERT_VENUE" => {
+                    ret.insert(
+                        event.id.unwrap(),
+                        serde_json::from_str(&event.payload).unwrap(),
+                    );
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        ret.into_values().collect()
+    }
+}
+
+fn from_json_str_with_id<'a, TTarget: Deserialize<'a>>(json_str: &str, id: Uuid) -> TTarget {
+    let mut value: serde_json::Value = serde_json::from_str(json_str).unwrap();
+    let id_value = serde_json::to_value(id);
+    value
+        .as_object_mut()
+        .unwrap()
+        .insert("id".to_owned(), id_value);
+    serde_json::from_value(value).unwrap()
+}
--- xtask/src/seed/mod.rs
+++ xtask/src/seed/mod.rs
@@ -1,2 +1,28 @@
+use std::path::PathBuf;
+
+use serde::de::DeserializeOwned;
+use tokio::fs::read_to_string;
+
+fn workspace_root_directory() -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("..");
+    path
+}
+
+fn json_seed_file_path(file_name_root: &str) -> PathBuf {
+    let mut path = workspace_root_directory();
+    path.push(&format!("phishin-seed/{file_name_root}.json"));
+    path
+}
+
+async fn parse_json_file<TTarget: DeserializeOwned>(
+    file_name_root: &str,
+) -> anyhow::Result<TTarget> {
+    Ok(serde_json::from_str(
+        &read_to_string(json_seed_file_path(file_name_root)).await?,
+    )?)
+}
+
 pub async fn seed() -> anyhow::Result<()> {
+    unimplemented!()
 }
