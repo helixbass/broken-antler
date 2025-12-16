fd3cb8dfb099386ce8376f6700fa48858027db12
Julian Rosse <julian@helixbass.net>
2025-12-16 15:44:01 -0500

    working on seed

--- xtask/src/add_ids.rs
+++ xtask/src/add_ids.rs
@@ -1,3 +1,20 @@
+use crate::{json_seed_file_path, parse_json_file};
+
+use tokio::fs;
+
 pub async fn add_ids(file_name_root: &str) -> anyhow::Result<()> {
-    unimplemented!()
+    let initial_json: serde_json::Value = parse_json_file(file_name_root).await.unwrap();
+    let nested_list = initial_json
+        .as_object()
+        .unwrap()
+        .get(file_name_root)
+        .unwrap();
+    fs::write(
+        json_seed_file_path(file_name_root),
+        serde_json::to_string_pretty(nested_list).unwrap(),
+    )
+    .await
+    .unwrap();
+
+    Ok(())
 }
