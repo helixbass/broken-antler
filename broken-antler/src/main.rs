01bb8af72340ed042f450fab5e8b5baa29f79a8d
Julian Rosse <julian@helixbass.net>
2025-12-17 14:55:11 -0500

    working on seed

--- broken-antler/src/main.rs
+++ broken-antler/src/main.rs
@@ -1,43 +1,31 @@
 use std::fmt;
-use std::sync::{
-    mpsc::{self, Sender},
-    Arc,
-};
+use std::sync::{mpsc::Sender, Arc};
 use std::task::{Context, Poll};
-use std::thread;
 
 use sauvignon_axum::{
-    axum::{
-        self,
-        extract::Request,
-        middleware::{self, Next},
-        response::Response,
-        Extension,
-    },
+    axum::{self, extract::Request, middleware::Next, response::Response},
     simple_app,
 };
 use tokio::net::TcpListener;
 use tower::{Layer, Service};
-use tracing_chrome::{ChromeLayerBuilder, TraceStyle};
-use tracing_subscriber::{prelude::*, EnvFilter};
 
 use broken_antler::{get_database, get_schema};
 use shared::get_db_pool;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let (chrome_layer, guard) = ChromeLayerBuilder::new().build();
-    tracing_subscriber::registry()
-        .with(chrome_layer)
-        .with(EnvFilter::new("debug"))
-        .init();
+    // let (chrome_layer, guard) = ChromeLayerBuilder::new().build();
+    // tracing_subscriber::registry()
+    //     .with(chrome_layer)
+    //     .with(EnvFilter::new("debug"))
+    //     .init();
 
-    let (trace_restarter_sender, trace_restarter_receiver) = mpsc::channel::<()>();
-    let _trace_restarter_handle = thread::spawn(move || {
-        for _message in trace_restarter_receiver.iter() {
-            guard.start_new(None);
-        }
-    });
+    // let (trace_restarter_sender, trace_restarter_receiver) = mpsc::channel::<()>();
+    // let _trace_restarter_handle = thread::spawn(move || {
+    //     for _message in trace_restarter_receiver.iter() {
+    //         guard.start_new(None);
+    //     }
+    // });
 
     let schema = get_schema();
     let db_pool = get_db_pool().await?;
@@ -45,15 +33,16 @@ async fn main() -> anyhow::Result<()> {
 
     axum::serve(
         TcpListener::bind("0.0.0.0:3002").await?,
-        simple_app(Arc::new(schema), Arc::new(database)).layer(RestartTraceLayer {
-            sender: trace_restarter_sender,
-        }),
+        simple_app(Arc::new(schema), Arc::new(database)), // .layer(RestartTraceLayer {
+                                                          //     sender: trace_restarter_sender,
+                                                          // }),
     )
     .await?;
 
     Ok(())
 }
 
+#[allow(dead_code)]
 #[derive(Clone)]
 struct RestartTraceLayer {
     pub sender: Sender<()>,
@@ -95,6 +84,7 @@ where
     }
 }
 
+#[allow(dead_code)]
 async fn restart_trace_middleware(request: Request, next: Next) -> Response {
     let response = next.run(request).await;
 
