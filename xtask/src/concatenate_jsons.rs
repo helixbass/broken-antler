14b7e54d586a1c48084187059358f3744968b2f6
Julian Rosse <julian@helixbass.net>
2025-12-17 12:42:54 -0500

    working on seed

--- xtask/src/concatenate_jsons.rs
+++ xtask/src/concatenate_jsons.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use regex::Regex;
+use squalid::_d;
 use tokio::fs;
 
 use crate::{json_seed_file_directory, json_seed_file_path, parse_json_file};
@@ -6,7 +10,7 @@ use crate::{json_seed_file_directory, json_seed_file_path, parse_json_file};
 pub async fn concatenate_jsons(file_name_root: &str) -> anyhow::Result<()> {
     let mut all_json_file_paths = fs::read_dir(json_seed_file_directory()).await.unwrap();
     let regex = Regex::new(&format!(r#"^{file_name_root}-\d+\.json"#)).unwrap();
-    let mut concatenated = serde_json::Value::Array(vec![]);
+    let mut parsed_files: HashMap<String, serde_json::Value> = _d();
     while let Some(dir_entry) = all_json_file_paths.next_entry().await.unwrap() {
         let file_name_str = dir_entry.file_name().into_string().unwrap();
         if regex.is_match(&file_name_str) {
@@ -14,16 +18,27 @@ pub async fn concatenate_jsons(file_name_root: &str) -> anyhow::Result<()> {
                 parse_json_file(&file_name_str.split(".").next().unwrap())
                     .await
                     .unwrap();
-            concatenated
-                .as_array_mut()
-                .unwrap()
-                .extend(one_file_json.as_array().unwrap().into_iter().cloned());
+            parsed_files.insert(file_name_str, one_file_json);
         }
     }
-    assert!(!concatenated.as_array().unwrap().is_empty());
+    assert!(!parsed_files.is_empty());
     fs::write(
         json_seed_file_path(file_name_root),
-        serde_json::to_string_pretty(&concatenated).unwrap(),
+        serde_json::to_string_pretty(&serde_json::Value::Array(
+            parsed_files
+                .into_iter()
+                .sorted_by_key(|(file_name, _)| file_name.clone())
+                .flat_map(|(_, one_file_json)| {
+                    one_file_json
+                        .as_array()
+                        .unwrap()
+                        .into_iter()
+                        .cloned()
+                        .collect::<Vec<_>>()
+                })
+                .collect(),
+        ))
+        .unwrap(),
     )
     .await
     .unwrap();
